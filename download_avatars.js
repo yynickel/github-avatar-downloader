6a627c38162068002f34ed8a7d71cdeab3f57376
Nick Yu <yynickel@gmail.com>
2019-06-03 21:54:42 +0000

    downloadImageByUrl implemented

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+let fs = require('fs');
 
 let token = require('./secret');
 
@@ -15,10 +16,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  let avatarUrls = result.map(x => x.avatar_url);
-  console.log("Result:", avatarUrls);
-});
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log(response, response.statusMessage, response.headers['content-type']);
+      console.log("Downloading image...");
+    })
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function() {
+      console.log("Download complete.");
+    })
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
+
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   let avatarUrls = result.map(x => x.avatar_url);
+//   console.log("Result:", avatarUrls);
+// });
 
 console.log('Welcome to the GitHub Avatar Downloader!');

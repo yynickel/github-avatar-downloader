2921d2fb9aa9f6a62adaa94e99eb1b2333a40fe7
Nick Yu <yynickel@gmail.com>
2019-06-03 21:22:17 +0000

    step 4

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,15 @@
 var request = require('request');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-
 
 console.log('Welcome to the GitHub Avatar Downloader!');
